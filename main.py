547a3a242b04a69752f68eb76b5f05e89f20eb8b
Felipe Martinez <felimart71@gmail.com>
2024-05-27 19:37:58 -0400

    Added get_song functionality, added more songs get_song functionality takes the meta_data from the song and changes it so it can be put in tracks.py (cleans the music metadata)

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ cl = Client()
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 def Login(): 
-    cl.login(USERNAME, PASSWORD)
+  cl.login(USERNAME, PASSWORD)
 
 # Upload photo
 photos = ['assets/post_pics/hugging.png', 'assets/post_pics/zuck_alien.png', 'assets/post_pics/zuck_portrait_dark.jpg', 'assets/post_pics/zuck_portrait_light.png']
 random_photo_path = random.choice(photos) # Photo to be uploaded
-#510 chars
+# 510 chars
 caption = "Hello again fellas, it's time to get off Instagram.\nPROTIP: Like this post so the algorithm gives you more of these posts in the future.\n.\n.\n.\nHASHTAGS: #getoffinstagram #addiction #fyp #foryou #explore #reels #andrewtate #samsulek #biden #trump #monkey #monkeybrain #dopamine #scrolling #stopscrolling #coolvideo #funny #cars #videogames #fortnite #rocketleague #leagueoflegends #lol #gta5 #gta6 #civ6 #minecraft #eveonline #epicgames #taylorswift #lanadelrey #kendricklamar #drake #suffering #takeabreak"
 def Upload_Photo():
     cl.photo_upload(random_photo_path, caption)
@@ -27,13 +27,28 @@ def Upload_Photo():
 reels = ['assets/reel_vids/car_reel.mp4', 'assets/reel_vids/zuck_reel.mp4']
 random_reel_path = random.choice(reels)
 thumbnail_path = 'assets/thumbnail.jpg'
-#usertags = [Usertag(user=USERNAME, x=0.1, y=0.1)]
-#location = Location(name='Russia, Saint-Petersburg', lat=59.96, lng=30.29)
+# usertags = [Usertag(user=USERNAME, x=0.1, y=0.1)]
+# location = Location(name='Russia, Saint-Petersburg', lat=59.96, lng=30.29)
 def Upload_Reel():
     cl.clip_upload(random_reel_path, caption, thumbnail_path)
 
 # Organize songs
-songs = [Murder, Rokstarr, aisatsana]
+#ADD: Still Yung lean, Pray 4 Me Slimesito
+songs = [
+    Murder,
+    Rokstarr,
+    aisatsana,
+    Breathe,
+    ICE,
+    Pretty_Girls_Like_Anime,
+    Victorious,
+    THROW_UP,
+    Notes_From_A_Wrist,
+    All_I_Need,
+    Cities, 
+    Let_Go,
+    Purple_Substance
+]
 # Generating random number to choose song for reel
 random_song = random.choice(songs)
 
@@ -44,7 +59,7 @@ def Upload_Reel_Music():
   cl.clip_upload_as_reel_with_music(random_reel_path, caption, random_song)
 
 
-#commenting out the sleeps to test if my account will get suspended again after I sucessfully appealed against my account suspension
+# commenting out the sleeps to test if my account will get suspended again after I sucessfully appealed against my account suspension
 # Control functions to be run
 def Run_IG_Posting():
   print("Posting on Instagram...")
@@ -57,13 +72,15 @@ def Run_IG_Posting():
 
 # Find metadata for a song
 def Search_Music(song_to_search):
+    print("Searching music...")
     Login()
     #song_to_search = ""
     print(cl.search_music(song_to_search))
 
+def main():
+    #Run_IG_Posting()
+
+    Search_Music("purple substance") # <--- Put song to search here
 
 if __name__ == "__main__":
-    Run_IG_Posting()
-
-    #print("Seaching music...")
-    #Search_Music('Candy Shop - 50 cent') # <--- Put song to search here
+  main()
