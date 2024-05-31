d39aafbdd8c5569fc5d594104c9c984db2eac142
Felipe Martinez <felimart71@gmail.com>
2024-05-31 02:29:30 -0400

    Getting song on run No longer storing song metadata in a file since it expires.

--- get_song.py
+++ get_song.py
@@ -1,27 +1,44 @@
-def clean_metadata(file_path):
-    with open(file_path, 'r') as f:
-        lines = f.readlines()
+import random
+from instagrapi import Client
 
-    with open(file_path, "w") as f:
-        for line in lines:
-            if line.strip("\n") != "Searching for music...":
-                line = (
-                    line.replace("[Track(id=", "Track(\n\tid=")
-                    .replace(", ", ",\n\t")
-                    .replace("Url(", "")
-                    .replace(")", "")
-                    .replace(
-                        "territory_validity_periods={}", "territory_validity_periods={}\n\t)"
-                    )
-                )
-                f.write(line)
+# Organize songs
+song_names = [
+    "Murder Sh!t - BIGBABYGUCCI",
+    "Rokstarr - Dro Kenji",
+    "aisatsana[102] - Aphex Twin",
+    "Breathe - Yeat",
+    "ICEWHORE! - Lumi Athena",
+    "Pretty Girls Like Anime - Kid J",
+    "Victorious - Yung Lean, Bladee",
+    "THROW UP - Dro Kenji",
+    "Notes From A Wrist - d4vid",
+    "All I Need - Slushii, Aviella",
+    "Cities - Throttle",
+    "Let Go - Duskus",
+    "Purple Substance - lil obnoxious",
+    "Cut - Duskus",
+    "Still - Yung Lean, Bladee",
+    "Pray 4 Me - Slimesito, Trippie Redd"
+]
+# Generating random number to choose song for reel
+random_song_name = random.choice(song_names)
 
-def main():
-  print("Cleaning...")
-  file_path = "./songs_data.txt"
-  clean_metadata(file_path)
-  # print(clean_metadata)
-  print("Cleaned music metadata!")
+# Retrieve metadata for a song given a song name
+def get_metadata(song_title):
+    print("Retrieving metadata for " + song_title + "...")
+    # Fetch the latest metadata for the song
+    new_metadata = cl.search_music(song_title)[0]
+    return new_metadata
 
-if __name__ == "__main__":
-  main()
+# Clean metadata from song so it can be used for instagrapi
+def clean_metadata(metadata):
+    metadata_str = str(metadata)
+    metadata_str = metadata_str.replace("Url(", "").replace(")", "").replace("territory_validity_periods={}", "territory_validity_periods={})")
+    # Revert metadata_str from str to object
+    metadata_obj = eval(metadata_str)
+    return metadata_obj
+
+def Get_Music():
+  print("Getting music for reel...")
+  song_metadata = get_metadata(random_song_name)
+  random_song = clean_metadata(song_metadata)
--- main.py
+++ main.py
@@ -5,14 +5,15 @@ import random # Used to choose a random pic for post and song for reel
 from instagrapi import Client
 
 # Import music tracks from tracks.py module
-from tracks import *
-
+# from tracks import *
+from get_song import Get_Music
 
 # Login to client
 cl = Client()
 USERNAME = os.getenv('USERNAME')
 PASSWORD = os.getenv('PASSWORD')
 def Login(): 
+  print("Logging in...")
   cl.login(USERNAME, PASSWORD)
 
 # Upload photo
@@ -21,7 +22,8 @@ random_photo_path = random.choice(photos) # Photo to be uploaded
 # 414 chars
 caption = "Hello fellas, it's time to get off Instagram.\nPROTIP: Like this post so the algorithm gives you more of these posts in the future.\n.\n.\n.\n#getoffig #addiction #fyp #explore #reels #andrewtate #samsulek #biden #trump #monkeybrain #dopamine #scrolling #stopscrolling #funny #cars #videogames #fortnite #rocketleague #leagueoflegends #lol #gta5 #gta6 #civ6 #minecraft #eveonline #taylorswift #kendricklamar #drake"
 def Upload_Photo():
-    cl.photo_upload(random_photo_path, caption)
+  print("Uploading photo...")
+  cl.photo_upload(random_photo_path, caption)
 
 # Upload a reel
 reels = ['assets/reel_vids/car_reel.mp4', 'assets/reel_vids/zuck_reel.mp4']
@@ -32,28 +34,9 @@ thumbnail_path = 'assets/thumbnail.jpg'
 def Upload_Reel():
     cl.clip_upload(random_reel_path, caption, thumbnail_path)
 
-# Organize songs
-#ADD: Still Yung lean, Pray 4 Me Slimesito
-songs = [
-    # Murder,
-    # Rokstarr,
-    # aisatsana,
-    Breathe,
-    ICE,
-    Pretty_Girls_Like_Anime,
-    Victorious,
-    THROW_UP,
-    Notes_From_A_Wrist,
-    All_I_Need,
-    Cities, 
-    Let_Go,
-    Purple_Substance
-]
-# Generating random number to choose song for reel
-random_song = random.choice(songs)
-
 # Upload a reel with music
 def Upload_Reel_Music():
+  print("Uploading reel...")
   if random_reel_path == 'assets/reel_vids/zuck_reel.mp4':
     cl.clip_upload_as_reel_with_music(random_reel_path, caption, Candy_Shop)
   else:
@@ -62,25 +45,15 @@ def Upload_Reel_Music():
 
 # Control functions to be run
 def Run_IG_Posting():
-  print("Posting on Instagram...")
+  print("Running bot to post on Instagram...")
   Login()
-  print("Logged in! Uploading photo...")
   Upload_Photo()
-  print("Uploading reel...")
+  Get_Music()
   Upload_Reel_Music()
 
 
-# Find metadata for a song
-def Search_Music(song_to_search):
-    print("Searching music...")
-    Login()
-    #song_to_search = ""
-    print(cl.search_music(song_to_search))
-
 def main():
-    Run_IG_Posting()
-
-    #Search_Music("purple substance") # <--- Put song to search here
+  Run_IG_Posting()
 
 if __name__ == "__main__":
   main()
