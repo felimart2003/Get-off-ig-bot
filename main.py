aec07047840b6635784136bfce7e62edba28bda8
Felipe Martinez <felimart71@gmail.com>
2024-06-14 16:06:12 -0400

    Added songs and Search Music functionality

--- main.py
+++ main.py
@@ -1,59 +1,88 @@
-import os # Github USERNAME, PASSWORD secret
-import random # Used to choose a random pic for post and song for reel
+import os  # Github USERNAME, PASSWORD secret
+import random  # Used to choose a random pic for post and song for reel
 
 # Import instagrapi - 3rd part IG API
 from instagrapi import Client
 
 # Import music utilities from song_utils.py module
-from song_utils import SongUtils
+import song_utils
+
 
 # Login to client
 cl = Client()
-USERNAME = os.getenv('USERNAME')
-PASSWORD = os.getenv('PASSWORD')
-def Login(): 
-  print("Logging in...")
-  cl.login(USERNAME, PASSWORD)
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+
+def Login():
+    print("Logging in...")
+    cl.login(USERNAME, PASSWORD)
+
 
 # Upload photo
-photos = ['assets/post_pics/hugging.png', 'assets/post_pics/zuck_alien.png', 'assets/post_pics/zuck_portrait_dark.jpg', 'assets/post_pics/zuck_portrait_light.png']
-random_photo_path = random.choice(photos) # Photo to be uploaded
+photos = [
+    "assets/post_pics/hugging.png",
+    "assets/post_pics/zuck_alien.png",
+    "assets/post_pics/zuck_portrait_dark.jpg",
+    "assets/post_pics/zuck_portrait_light.png",
+]
+random_photo_path = random.choice(photos)  # Photo to be uploaded
 # 414 chars
 caption = "Hello fellas, it's time to get off Instagram.\nPROTIP: Like this post so the algorithm gives you more of these posts in the future.\n.\n.\n.\n#getoffig #addiction #fyp #explore #reels #andrewtate #samsulek #biden #trump #monkeybrain #dopamine #scrolling #stopscrolling #funny #cars #videogames #fortnite #rocketleague #leagueoflegends #lol #gta5 #gta6 #civ6 #minecraft #eveonline #taylorswift #kendricklamar #drake"
-def Upload_Photo():
-  print("Uploading photo...")
-  cl.photo_upload(random_photo_path, caption)
 
-# Upload a reel
-reels = ['assets/reel_vids/car_reel.mp4', 'assets/reel_vids/zuck_reel.mp4']
+
+def Upload_Photo():
+    print("Uploading photo...")
+    cl.photo_upload(random_photo_path, caption)
+
+
+# Upload a reel (with original audio)
+reels = ["assets/reel_vids/car_reel.mp4", "assets/reel_vids/zuck_reel.mp4"]
 random_reel_path = random.choice(reels)
-thumbnail_path = 'assets/thumbnail.jpg'
+thumbnail_path = "assets/thumbnail.jpg"
+
+
 # usertags = [Usertag(user=USERNAME, x=0.1, y=0.1)]
 # location = Location(name='Russia, Saint-Petersburg', lat=59.96, lng=30.29)
 def Upload_Reel():
-  cl.clip_upload(random_reel_path, caption, thumbnail_path)
+    print("Uploading normal reel...")
+    cl.clip_upload(random_reel_path, caption, thumbnail_path)
+
 
 # Upload a reel with music
 def Upload_Reel_Music():
-  print("Uploading reel...")
-  song_tools = SongUtils(cl) # Initialize song_utils with instagrapi
-  if random_reel_path == 'assets/reel_vids/zuck_reel.mp4':
-    track_metadata = song_tools.get_music("Candy Shop - 50 Cent")
-    track_metadata.highlight_start_times_in_ms = [4100]
-    cl.clip_upload_as_reel_with_music(random_reel_path, caption, track_metadata)
-  else:
-    track_metadata = song_tools.get_music(song_tools.get_random_song())
-    cl.clip_upload_as_reel_with_music(random_reel_path, caption, track_metadata)
+    print("Uploading reel with music...")
+    try:
+        song_tools = song_utils.SongUtils(cl)  # Initialize song_utils with instagrapi
+        if random_reel_path == "assets/reel_vids/zuck_reel.mp4":
+            track_metadata = song_tools.get_music("Candy Shop - 50 Cent")
+            track_metadata.highlight_start_times_in_ms = [3925]
+            cl.clip_upload_as_reel_with_music(random_reel_path, caption, track_metadata)
+        else:
+            # track_metadata = song_tools.get_music(song_tools.get_random_song())
+            ran_song = song_utils.random_song
+            track_metadata = song_tools.get_music(ran_song)
+            if track_metadata == []:
+                print(f"Could not find track for {ran_song}")
+            cl.clip_upload_as_reel_with_music(random_reel_path, caption, track_metadata)
+    except:
+        print("Failed to upload reel with music, uploading normal reel instead...")
+        Upload_Reel()
 
-# Control functions to be run
+
+# Control functions to be run on IG Posting
 def Run_IG_Posting():
-  print("Running bot to post on Instagram...")
-  Login()
-  # Upload_Photo()
-  Upload_Reel_Music()
-  
+    print("Running bot to post on Instagram...")
+    Login()
+    Upload_Photo()
+    Upload_Reel_Music()
+    print("Done.")
+
+
 def main():
     Run_IG_Posting()
+    # Login()
+    # song_utils.SongUtils(cl).get_music_metadata("PINK FLOYD - JADY'S BIRTHDAY")
 
 if __name__ == "__main__":
-  main()
+    main()
--- song_utils.py
+++ song_utils.py
@@ -17,9 +17,45 @@ class SongUtils:
         "Cities - Throttle",
         "Let Go - Duskus",
         "Purple Substance - lil obnoxious",
+        "Still - Yung Lean, Bladee",
+        "Fall Back - Lithe",
         "Cut - Duskus",
         "Still - Yung Lean, Bladee",
-        "Pray 4 Me - Slimesito, Trippie Redd"
+        "Pray 4 Me - Slimesito, Trippie Redd",
+        "Ceremony - Skrillex, Yung Lean, Bladee",
+        "Wings - Shoreline Mafia",
+        "Hot Girl - OHGEESY",
+        "bellyache - Billie Eilish",
+        "Stash - Casper TNG, 6ixbuzz",
+        "TRANCE - Dro Kenji",
+        "RENOUND - Dro Kenji",
+        "5% TINT - Travis Scott",
+        "Stuck in a Dream - Pardyalone",
+        "trix - whiterosemoxie",
+        "beauty - mgk, Trippie Redd",
+        "Motley Crew - Post Malone",
+        "THE LINE (feat. d4vd) - The Kid LAROI, d4vd",
+        "T.D (Lil Yachty & Tierra Whack feat. A$AP Rocky & Tyler, The Creator) - Lil Yachty, Tierra Whack, A$AP Rocky, Tyler, The Creator",
+        "Missles (feat. Trippie Redd) - Lil Gnar, Trippie Redd",
+        "Weeeeee - Trippie Redd",
+        "3 Nights - Dominic Fike",
+        "Phone Numbers - Dominic Fike, Kenny Beats",
+        "Mob Ties - Drake",
+        "Moonwalking in Calabasas (feat. Blueface) - Remix - DDG, Blueface",
+        "Invincible - Pop Smoke",
+        "Costa Rica - Bankrol Hayden",
+        "Alpha - Guapdad 4000",
+        "Dubai Shit - Huncho Jack, Travis Scott",
+        "All the Smoke - Tyla Yaweh",
+        "jack money bean - bbno$, Yung Gravy, lentra",
+        "Red Room - Offset",
+        "MAFIA - Travis Scott",
+        "Falling Out of Love (feat. CashBently) - 909memphis, Cash Bently",
+        "Hero - Tom The Mail Man",
+        "Adding - Yung Fazo, SoFaygo",
+        "Hit My Dance - Mills",
+        "Vroom Vroom - Logic, C Dot Castro, halfBREED",
+        "PINK FLOYD - JADY'S BIRTHDAY"
     ]
 
     @staticmethod
@@ -28,6 +64,13 @@ class SongUtils:
     
     def __init__(self, client):
         self.client = client
+
+    def get_music_metadata(self, song_to_search):
+        print("Searching music...")
+        # song_to_search = ""
+        song_metadata_info = self.client.search_music(song_to_search)
+        print(song_metadata_info)
+        return song_metadata_info
 
     # Returns song metadata to use in Reels given a Str with the name and artist of song
     def get_music(self, song_title):
